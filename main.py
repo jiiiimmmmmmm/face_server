a221bd39ce1e3a00633f17694060d3e892b97616
jiiiimmmmmmm <37611620+jiiiimmmmmmm@users.noreply.github.com>
2021-03-29 18:13:40 +0800

    change function into classes
    把功能函数封装成类

--- main.py
+++ main.py
@@ -7,57 +7,34 @@ import matplotlib.pyplot as plt
 from utils import * 
 app = Flask(__name__)
 
-keras_file = "./model/facenet_keras.h5"
-keras_model = loadKerasModel(keras_file)
-annoy = AnnoyIndex(128, metric='angular')
-annoy.load('./annoyIndex/face_vector.nn')
+manager = face_manager(database_path="mysql://root:999123ac@127.0.0.1/student_exam")
 
 @app.route('/search', methods=['POST','GET'])
 def search():
     data = request.data
     data = json.loads(data)
+    print(type(data['imageBase64']))
     image = base64.b64decode(data['imageBase64'])
     image = np.fromstring(image, np.uint8)
     image = image.reshape([data['height'],data['width'],data['channel']])
     image = image[:,:,:3]
 
+    cv2.imwrite('temp.png', image)
 
-    imageBGR = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    img2gray = cv2.cvtColor(imageBGR, cv2.COLOR_BGR2GRAY)
-    imageVar = cv2.Laplacian(img2gray, cv2.CV_64F).var()
-    print('清晰度：'+str(imageVar))
-    cv2.imwrite('temp.png',imageBGR)
+    result = manager.face_nearest_face(image)
+    print(result)
 
 
-    e = embedding(keras_model, image)
-    print(annoy.get_nns_by_vector(e,5,include_distances=True))
-    [id,dis] = annoy.get_nns_by_vector(e,1,include_distances=True)
-
-    if dis[0] < 0.6:
-        if id[0] < 4:
-            res = {
-                'msg': 'success',
-                'id': '20172131096',
-                'name': '吴梓祺',
-                'exam': '计算机原理',
-                'room': '主教学楼 西340',
-                'time': '9:30-11:30'
-            }
-        else:
-            res = {
-                'msg': 'success',
-                'id': '20172131053',
-                'name': '朱晓健',
-                'exam': '网络工程',
-                'room': '南教学楼 西222',
-                'time': '10:00-12:30'
-            }
-        return jsonify(res), 200 
-    else:
-        res = {
-            'msg': 'unkown',
+    res = {
+            'msg': 'success',
+            'id': '20172131096',
+            'name': '吴梓祺',
+            'exam': '计算机原理',
+            'room': '主教学楼 西340',
+            'time': '9:30-11:30'
         }
-        return jsonify(res), 201
+    return jsonify(res), 200 
+
 
 
 @app.route('/welcome')
--- utils.py
+++ utils.py
@@ -11,97 +11,182 @@ import glob
 import tensorflow as tf
 from keras.models import load_model
 from annoy import AnnoyIndex
+from mtcnn import MTCNN
+import matplotlib.pyplot as plt
 
 
-def loadKerasModel(file):
-    model = load_model(file, compile=False)
-    return model
 
-def makeSquare(face):
-    h,w,_ = face.shape
-    if w>h:
-        start = int((w-h)/2)
-        end = start+h
-        return face[:,start:end,:]
-    else:
-        start = int((h-w)/2)
-        end = start+w
-        return face[start:end,:,:]
+class faceHelper:
+    @staticmethod
+    def makeSquare(face):
+        h,w,_ = face.shape
+        if w>h:
+            start = int((w-h)/2)
+            end = start+h
+            return face[:,start:end,:]
+        else:
+            start = int((h-w)/2)
+            end = start+w
+            return face[start:end,:,:]
 
-def preprocess(face, required_size=(160, 160)):
-    face = makeSquare(face)
-    face = cv2.resize(face, required_size)
-    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-    # standardize pixel values across channels (global)
-    ret = (np.float32(face) - 127.5) / 127.5
-    ret = ret.reshape([-1,*required_size,3])
-    return ret
+    @classmethod
+    def preprocess_single_RGB_image(cls, face, required_size=(160,160)):
+        assert face.shape[-1] == 3
+        face = cls.makeSquare(face)
+        face = cv2.resize(face, required_size)
+        face = (np.float32(face) - 127.5) / 127.5
+        face = np.expand_dims(face, 0)
+        return face
 
-
-def printAllFromLmdb():
-    with lmdb.open('./lmdb') as env:
-        wfp = env.begin()
-        for key,value in wfp.cursor():
-            print(decode(key,value))
-
-def encode(key,vector):
-    key = struct.pack('H',key)
-    value = struct.pack('128f',*vector)
-    return key,value
-def decode(key, value):
-    key = struct.unpack('H', key)
-    vector = struct.unpack('128f',value)
-    return key,vector
-def writeEmbeddingsToLmdb(path):
-    imagePaths = glob.glob(path)
-    print(','.join([path.split('\\')[-1] for path in imagePaths]))
-    embs = []
-    for imagePath in imagePaths:
-        image = cv2.imread(imagePath)
-        image = preprocess(image)
-        emb = keras_model.predict(image)
-        emb /= np.linalg.norm(emb)
-        embs.append(emb[0])
-    embs = np.stack(embs)
-    with lmdb.open('./lmdb') as env:
-        wfp = env.begin(write=True)
-        for index,emb in enumerate(embs):
-            key,value = encode(index, emb)
-            wfp.put(key,value)
-        wfp.commit()
+    @staticmethod
+    def encode_embedding(vector, length = 128):
+        value = struct.pack('{}f'.format(length),*vector)
+        return value
+    @staticmethod
+    def decode_embedding(value, length = 128):
+        vector = struct.unpack('{}f'.format(length), value)
+        return vector
 
 
-def embedding(model, faceImage):
-    sample = preprocess(faceImage)
-    emb = model.predict(sample)[0]
-    emb /= np.linalg.norm(emb)
-    return emb
 
-def createAnnoyIndex():
-    annoy = AnnoyIndex(128, metric='angular')
-    with lmdb.open('./lmdb') as env:
-        wfp = env.begin()
-        for key, value in wfp.cursor():
-            key,vector = decode(key,value)
-            annoy.add_item(key[0],vector)
-    annoy.build(10)
-    if not os.path.exists('./annoyIndex/'):
-        os.mkdir('./annoyIndex/')
-    annoy.save('./annoyIndex/face_vector.nn')
-    print('create annoy index successfully')
+class faceEmbedding:
+    def __init__(self, model_path):
+        """初始化 载入模型"""
+        self.model = load_model(model_path, compile=False)
+    
+    def get_face_embedding(self, face):
+        face = faceHelper.preprocess_single_RGB_image(face)
+        embedding =  self.model.predict(face)
+        embedding /= np.linalg.norm(embedding)
+        return embedding[0]
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, DateTime, String, ForeignKey, BINARY, desc
+from sqlalchemy.orm import sessionmaker
+
+Base  = declarative_base()
+class StudentFace(Base):
+        __tablename__ = 'student_face'
+
+        id = Column(Integer, primary_key=True)
+        _face_embedding = Column('face_embedding',BINARY(12), nullable=False)
+        student_id = Column(Integer , nullable=False, index=True)  
+
+        def __init__(self, id, face_embedding, student_id):
+            self.id = id
+            self._face_embedding = faceHelper.encode_embedding(face_embedding)
+            self.student_id = student_id
+
+        @property
+        def face_embedding(self):
+            return faceHelper.decode_embedding(self._face_embedding)
+
+class faceDetection:
+    def __init__(self):
+        self.model = MTCNN()
+    
+    def detect_face_from_RGB(self, face_image):
+        result = self.model.detect_faces(face_image)
+        if len(result) == 1 and result[0]['confidence'] > 0.99:
+            x,y,w,h = map(lambda value: abs(value), result[0]['box'])
+            return face_image[y:y+h, x:x+w]
+        else:
+            return np.array([])
+    
+class face_manager:
+
+    def __init__(self, database_path, face_net_model_path = 'model/facenet_keras.h5', annoy_index_dir='annoyIndex'):
+        engine = create_engine(database_path)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        self.face_embedding = faceEmbedding(face_net_model_path)
+        self.face_detector = faceDetection()
+        self.last_id = self.session.query(StudentFace.id).order_by(desc(StudentFace.id)).first()
+        self.last_id = self.last_id[0] if self.last_id else 0
+        self.annoy = None
+        self.annoy_index_dir = annoy_index_dir
+
+
+    def add_face(self, face_image, student_id):
+        face_image = self.face_detector.detect_face_from_RGB(face_image)
+        if face_image.shape == (0,):
+            print('do not detect any face')
+            return face_image
+        embedding = self.face_embedding.get_face_embedding(face_image)
+        stuFace = StudentFace(self.last_id+1 , embedding, student_id)
+        try:
+            self.session.add(stuFace)
+            self.session.commit()
+            self.last_id += 1
+            return face_image
+        except Exception as e:
+            print(e.__traceback__)
+            print(e)
+            self.session.rollback()
+
+    def create_annoy_index(self, vector_length=128, annoy_index_dir='annoyIndex', tree_num=50):
+        self.vector_length = vector_length
+        self.annoy_index_dir = annoy_index_dir
+        self.tree_num = tree_num
+
+        annoy = AnnoyIndex(vector_length, metric='angular')
+        for student_face in self.session.query(StudentFace).all():
+            annoy.add_item(student_face.id, student_face.face_embedding)
+
+        annoy.build(tree_num)
+        self.annoy = annoy
+
+        if not os.path.exists(annoy_index_dir):
+            os.mkdir(annoy_index_dir)
+        annoy.save(os.path.join(annoy_index_dir,'face_vector.nn'))
+        print('create annoy index successfully')
+
+    def face_nearest_face(self, face_image, n_nearest=5, show_distance=True):
+        if not self.annoy:
+            self.annoy = AnnoyIndex(128, metric='angular')
+            self.annoy.load(os.path.join(self.annoy_index_dir,'face_vector.nn'))
+
+        face_image = self.face_detector.detect_face_from_RGB(face_image)
+        if face_image.shape == (0,) :
+            print('do not detect any face')
+            return None
+
+        embedding = self.face_embedding.get_face_embedding(face_image)
+        ids, distance = self.annoy.get_nns_by_vector(embedding, n_nearest ,include_distances=show_distance)
+        ids = [self.session.query(StudentFace.student_id).filter_by(id=id).first()[0] for id in ids]
+        return (ids, distance)
+
+
 
 if __name__ == '__main__':
-    keras_file = "./model/facenet_keras.h5"
-    keras_model = loadKerasModel(keras_file)
-    # writeEmbeddingsToLmdb('./image_cropped/*.jpg')
-    # printAllFromLmdb()
-    # createAnnoyIndex()
-    annoy = AnnoyIndex(128, metric='angular')
-    annoy.load('./annoyIndex/face_vector.nn')
-    face_image = cv2.imread('temp.png')
-    face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
-    e = embedding(face_image)
-    print(e)
-    res = annoy.get_nns_by_vector(e,5,include_distances=True)
-    print(res)
 
+    manager = face_manager(database_path="mysql://root:999123ac@127.0.0.1/student_exam")
+
+    student_image_paths = glob.glob('data/image/selected/*')
+
+    for student_image_path in student_image_paths:
+        face_image = cv2.imread(student_image_path)
+        face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
+        id = int(os.path.basename(student_image_path).split('_')[0])
+        face_image = manager.add_face(face_image, student_id=id)
+        
+        if face_image.shape != (0,):
+            print(id)
+            face_image = cv2.cvtColor(face_image, cv2.COLOR_RGB2BGR)
+            target = os.path.join('data/image/casia_facev5_cropped/',os.path.basename(student_image_path))
+            print(target)
+            cv2.imwrite(target, face_image)
+     
+    manager.create_annoy_index()
+
+    # image_paths = glob.glob('data/image/casia_facev5/*/*')
+    # image_paths = image_paths[:20*5]
+    # for i, image_path in enumerate(image_paths):
+    #     print(os.path.basename(image_path))
+    #     face_image = cv2.imread(image_path)
+    #     face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
+    #     result = manager.face_nearest_face(face_image)
+        
+    #     print(result)
